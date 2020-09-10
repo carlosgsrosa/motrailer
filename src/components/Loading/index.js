3e92efa24806a0e93b9afe57daaf58eafc91250b
Carlos Rosa <carlao.garcia@gmail.com>
2020-09-10 14:08:18 -0300

    Improvement: Add Lottie animation loading footer

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {ActivityIndicator} from 'react-native';
+import LottieView from 'lottie-react-native';
 
 import {HorizontalView} from '../';
 
 export default function Loading(props) {
-  const {color, size} = props;
   return (
     <HorizontalView
       paddingTop="15px"
       paddingBottom="15px"
       justifyContent="center">
-      <ActivityIndicator size={size} color={color} />
+      <LottieView
+        autoSize
+        source={require('../../modal/loading.json')}
+        autoPlay
+        loop
+      />
     </HorizontalView>
   );
 }
--- src/components/LoadingModal/index.js
+++ src/components/LoadingModal/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Modal} from 'react-native';
-import LottieView from 'lottie-react-native';
 
 import {VerticalView, Loading} from '../';
 
@@ -8,13 +7,7 @@ export default function LoadingModal({visible}) {
   return (
     <Modal visible={visible}>
       <VerticalView flex={1} justifyContent="center" alignItems="center">
-        {/* <Loading size="large" color="#ED7329" /> */}
-        <LottieView
-          autoSize
-          source={require('../../modal/loading.json')}
-          autoPlay
-          loop
-        />
+        <Loading />
       </VerticalView>
     </Modal>
   );
